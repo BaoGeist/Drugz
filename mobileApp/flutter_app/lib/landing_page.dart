7029ed7b857ee56ec09c945ea9a8fdc5ea38de41
Derron Li <104483680+derronli@users.noreply.github.com>
2023-01-15 01:02:21 -0500

    Added Navbar

---
+++ mobileApp/flutter_app/lib/landing_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("BODYBODYBODYBODY"),
+      ),
+    );
+  }
+}
--- mobileApp/flutter_app/lib/main.dart
+++ mobileApp/flutter_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter_app/camera_page.dart';
+import 'package:flutter_app/landing_page.dart';
+import 'package:path/path.dart';
 
 // Camera stuff I can't use yet
 // // Ensure that plugin services are initialized so that `availableCameras()`
@@ -13,16 +15,12 @@ import 'package:flutter_app/camera_page.dart';
 // // Get a specific camera from the list of available cameras.
 // final firstCamera = cameras.first;
 
-
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-
 
   // This widget is the root of your application.
   @override
@@ -65,6 +63,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _currIndex = 0;
+  List pages = [CameraPage(), LandingPage()];
 
   @override
   Widget build(BuildContext context) {
@@ -80,42 +80,70 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      body: pages[_currIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings")
+        ],
+        currentIndex: _currIndex,
+        onTap: (index) {
+          setState(() {
+            _currIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class CameraPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget> [ElevatedButton(
-            onPressed: () async {
-              await availableCameras().then(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () async {
+                  await availableCameras().then(
                     (value) => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => TakePictureScreen(
-                      // Pass the appropriate camera to the TakePictureScreen widget.
-                      camera: value.first,
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TakePictureScreen(
+                          // Pass the appropriate camera to the TakePictureScreen widget.
+                          camera: value.first,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              );
-            },
-            child: const Text('Launch Camera'),
-          ),
-        ]),
+                  );
+                },
+                child: const Text('Launch Camera'),
+              ),
+            ]),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }

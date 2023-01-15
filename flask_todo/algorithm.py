cb488a8dfc4ce345fa54b91556792ccd91efd62f
Baoze Lin <linofbaoze@outlook.com>
2023-01-15 06:21:22 -0500

    working http post request, and calling barcodes.py and algorithm.py; moved things for easier access; python main.py must be run from inside flask_todo

---
+++ flask_todo/algorithm.py
@@ -0,0 +1,61 @@
+import barcodes
+import pandas as pd
+import random
+
+# BIG NOTE THIS FILE ALWAYS NEEDS TO BE RUN FROM INSIDE FLASK_TODO
+
+def build_database(size):
+    list_barcode_id = []
+    list_drug_name = ["isotretinon", "Hydrocodone-Acetaminophen", "Hydrochlorothiazide", "Atorvastatin Calcium", "Ventolin"]
+    list_negative_interations = ['bad1, bad1, bad1', 'bad2 bad2 bad2', 'bad2 bad2 bad2', 'bad2 bad2 bad2', 'bad2 bad2 bad2']
+    for i in range (size-1):
+        list_barcode_id.append(str(random.randint(10000000, 99999999)))
+    list_barcode_id.append(str(59012344))
+
+    data = {'barcode_id':  list_barcode_id,
+        'drug_name': list_drug_name,
+         'negative_interactions': list_negative_interations
+        }
+
+    df = pd.DataFrame(data)
+    return df
+
+def check_medication(image, df):
+    string_return = ""
+    destination = int(barcodes.read_barcode(image))
+    # return for the name of the drug, comment out the other to use
+    string_return += "Drug Name:\n" + df.loc[df['Code'] == destination]['Name'].values[0]
+    # return([df.loc[df['Code'] == destination]['Name'].values], [(df.loc[df['Code'] == destination]['Conflicts'].values)])
+    # return for the interaction list of the drug, comment out the other to use
+    string_return += "\nConflicts: \n" + df.loc[df['Code'] == destination]['Conflicts'].values[0].replace(";", "; ")
+    return string_return
+
+def check_medication_from_outside(image_path):
+    string_return = ""
+    destination = int(barcodes.read_barcode(image_path))
+    df = build_database_real("drug_data.csv")
+    # return for the name of the drug, comment out the other to use
+    string_return += "Drug Name:\n" + df.loc[df['Code'] == destination]['Name'].values[0]
+    # return([df.loc[df['Code'] == destination]['Name'].values], [(df.loc[df['Code'] == destination]['Conflicts'].values)])
+    # return for the interaction list of the drug, comment out the other to use
+    string_return += "\nConflicts: \n" + df.loc[df['Code'] == destination]['Conflicts'].values[0].replace(";", "; ")
+    return string_return
+
+def build_database_real(csv_file_path):
+    df = pd.read_csv(csv_file_path)
+    return df
+
+def main():
+    # df = build_database(5)
+
+    df = build_database_real("drug_data.csv")
+    # list_codes = df.Code.values.tolist()
+    # for code in list_codes:
+        # barcodes.create_barcode(str(code), str(code))
+    print(check_medication("barcode_storage/6965131511316.png", df))
+
+# function call for a return string with med name and conflict name
+# print(check_medication_from_outside("barcode_storage/6965131511316.png"))
+
+# general main call
+# main()
---
+++ flask_todo/barcodes.py
@@ -0,0 +1,72 @@
+# imports ----------
+
+# import EAN13 from barcode module for EAN13 support
+from barcode import EAN13
+# import ImageWriter to generate an image file
+from barcode.writer import ImageWriter
+# computer vision to show the barcode image
+import cv2
+# pyzbar identify barcode borders in images and decodes them
+from pyzbar.pyzbar import decode
+
+
+# global variables ----------
+global barcode_storage_variable
+barcode_storage_variable = "Drugz/flask_todo/barcode_storage/"
+
+# create a barcode from a 13-digit code, and saves it as an image
+def create_barcode(number_id, image_name):
+    # Now, let's create an object of EAN8 class and 
+    # pass the number with the ImageWriter() as the 
+    # writer
+    print(number_id)
+    my_code = EAN13(number_id, writer=ImageWriter())
+    
+    # Our barcode is ready. Let's save it.
+    save_string = barcode_storage_variable + image_name
+    my_code.save(save_string)
+
+  
+# detects and decodes barcode
+def read_barcode(image):
+     
+    # read the image in numpy array using cv2
+    img = cv2.imread(image)
+      
+    # decode the barcode image
+    detectedBarcodes = decode(img)
+      
+    # if not detected then print the message
+    if not detectedBarcodes:
+        print("Barcode not detected")
+    else:
+       
+          # traverse through all the detected barcodes in image, and displays a rectangle over them
+        for barcode in detectedBarcodes: 
+            # locate the barcode position in image
+            (x, y, w, h) = barcode.rect
+             
+            # places rectangle over iamge
+            cv2.rectangle(img, (x-10, y-10),(x + w+10, y + h+10),(255, 0, 0), 2)
+            
+            # return if barcode data is valid
+            if barcode.data!="":
+                # NOTE show_image has been commented out but can be called
+                # show_barcodes(img)
+                return barcode.data.decode("utf-8")
+    
+def move_image(old_path, new_path):
+    pass
+    
+# function to show the image
+def show_barcodes(img):
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def main():
+    create_barcode('7632267416356', '7632267416356')
+    
+    # image="59012341.png"
+
+    # print(read_barcode(image))

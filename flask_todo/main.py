6c4d61b8b9787ae5f08009daf0282557b30ff181
ZaraKhan1252 <khanz91@mcmaster.ca>
2023-01-15 05:40:04 -0500

    completed first version of api

--- flask_todo/main.py
+++ flask_todo/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+import requests
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,8 +10,79 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 
 
-# suppress SQLALCHEMY_TRACK_MODIFICATIONS warning
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+class TodoItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    is_executed = db.Column(db.Boolean)
+
+    def __init__(self, name, is_executed):
+        self.name = name
+        self.is_executed = is_executed
+
+
+# Todo schema
+class TodoSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'is_executed')
+
+
+# Initialize schema
+todo_schema = TodoSchema()
+todos_schema = TodoSchema(many=True)
+
+@app.route('/test', methods=['POST'])
+def test():
+    data = request.form['data']
+    datastring = data+data
+    return datastring
+
+@app.route('/picture', methods=['POST'])
+def picture():
+    number = request.form['number']
+    return_string = "medicine yeah bad dont do"
+    return return_string
+
+@app.route('/todo', methods=['POST'])
+def add_todo():
+    name = request.json['name']
+    is_executed = request.json['is_executed']
+
+    new_todo_item = TodoItem(name, is_executed)
+    db.session.add(new_todo_item)
+    db.session.commit()
+
+    return todo_schema.jsonify(new_todo_item)
+
+@app.route('/todo', methods=['GET'])
+def get_todos():
+    all_todos = TodoItem.query.all()
+    result = todos_schema.dump(all_todos)
+
+    return jsonify(result)
+
+
+@app.route('/todo/<id>', methods=['PUT', 'PATCH'])
+def execute_todo(id):
+    todo = TodoItem.query.get(id)
+
+    todo.is_executed = not todo.is_executed
+    db.session.commit()
+
+    return todo_schema.jsonify(todo)
+
+
+@app.route('/todo/<id>', methods=['DELETE'])
+def delete_todo(id):
+    todo_to_delete = TodoItem.query.get(id)
+    db.session.delete(todo_to_delete)
+    db.session.commit()
+
+    return todo_schema.jsonify(todo_to_delete)
+
+if __name__ == '__main__':
+    app.run(debug=True)
